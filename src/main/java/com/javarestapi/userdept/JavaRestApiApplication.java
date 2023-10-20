2436182347e6b3578b561369b7de75dbb0d4342c
JohnDaCruz <batatadoce31@hotmail.com>
2023-10-20 02:09:06 -0300

    Rest API Java

---
+++ src/main/java/com/javarestapi/userdept/JavaRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.javarestapi.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class JavaRestApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestApiApplication.class, args);
+    }
+
+
+}
